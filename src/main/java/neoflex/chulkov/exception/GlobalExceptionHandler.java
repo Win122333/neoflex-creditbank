75175d60568e4fca6a8dce8be4fab9bc147908fb
Win122333 <artem.chulkov.90@mail.ru>
2026-03-17 20:13:10 +0300

    fix error message

--- src/main/java/neoflex/chulkov/exception/GlobalExceptionHandler.java
+++ src/main/java/neoflex/chulkov/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package neoflex.chulkov.exception;
 
 import neoflex.chulkov.dto.ErrorResponseDto;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,13 +13,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ScoringException.class)
     public ResponseEntity<ErrorResponseDto> handleScoringException(ScoringException e) {
         ErrorResponseDto response = new ErrorResponseDto(
-                HttpStatus.UNPROCESSABLE_ENTITY.value(), e.getMessage(), e.getErr().getMessage());
+                HttpStatus.UNPROCESSABLE_ENTITY.value(), e.getMessage(), "Не прошёл скоринг");
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(response);
     }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         ErrorResponseDto response = new ErrorResponseDto(
-                HttpStatus.BAD_REQUEST.value(), e.getMessage(), "Ошибка валидации");
+                HttpStatus.BAD_REQUEST.value(),
+                e.getFieldErrors().stream()
+                        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                        .reduce("", (x, z) -> x.concat(", ").concat(z))
+                , "Ошибка валидации");
+        //TODO
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
