63483016ced100efee46c95f472cdb3c5d1306e6
Win122333 <artem.chulkov.90@mail.ru>
2026-04-09 19:36:12 +0300

    generate statementApiDelegate

---
+++ statement/src/main/java/neoflex/chulkov/api/StatementApiDelegateImpl.java
@@ -0,0 +1,27 @@
+package neoflex.chulkov.api;
+
+import lombok.extern.slf4j.Slf4j;
+import neoflex.chulkov.dto.LoanOfferDto;
+import neoflex.chulkov.dto.LoanStatementRequestDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class StatementApiDelegateImpl implements StatementApiDelegate {
+    @Override
+    public ResponseEntity<List<LoanOfferDto>> statement(
+            LoanStatementRequestDto loanStatementRequestDto
+    ) {
+        return StatementApiDelegate.super.statement(loanStatementRequestDto);
+    }
+
+    @Override
+    public ResponseEntity<Void> select(
+            LoanOfferDto loanOfferDto
+    ) {
+        return StatementApiDelegate.super.select(loanOfferDto);
+    }
+}
