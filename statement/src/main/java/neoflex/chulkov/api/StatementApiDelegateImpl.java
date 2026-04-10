d6f33cffc919d63157ce5d805f20593c0bc12a91
Win122333 <artem.chulkov.90@mail.ru>
2026-04-10 13:11:17 +0300

    add RestClient

--- statement/src/main/java/neoflex/chulkov/api/StatementApiDelegateImpl.java
+++ statement/src/main/java/neoflex/chulkov/api/StatementApiDelegateImpl.java
@@ -1,6 +1,8 @@
 package neoflex.chulkov.api;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import neoflex.chulkov.client.DealRestClient;
 import neoflex.chulkov.dto.LoanOfferDto;
 import neoflex.chulkov.dto.LoanStatementRequestDto;
 import org.springframework.http.ResponseEntity;
@@ -10,14 +12,19 @@ import java.util.List;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class StatementApiDelegateImpl implements StatementApiDelegate {
+
+    private final DealRestClient dealRestClient;
+
     @Override
     public ResponseEntity<List<LoanOfferDto>> statement(
             LoanStatementRequestDto loanStatementRequestDto
     ) {
         log.info("called /statement with request = {}", loanStatementRequestDto);
+        List<LoanOfferDto> response = dealRestClient.getAvailableOffers(loanStatementRequestDto);
         log.info("Ответ: {}", response);
-        return response;
+        return ResponseEntity.ok(response);
     }
 
     @Override
---
+++ statement/src/main/java/neoflex/chulkov/config/RestClientConfig.java
@@ -0,0 +1,18 @@
+package neoflex.chulkov.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfig {
+    @Bean
+    public RestClient getRestClient(
+            @Value("${statement.deal-base-url:localhost:8083}") String baseUrl
+    ) {
+        return RestClient.builder()
+                .baseUrl(baseUrl)
+                .build();
+    }
+}
