9a0da3fa585fc44e81175e0851e7fccb552d4650
Win122333 <chulkov_a_d@sc.vsu.ru>
2026-05-02 21:56:32 +0300

    add pessimistic lock and SKIP LOCK

--- deal/src/main/java/neoflex/chulkov/repository/OutboxRepository.java
+++ deal/src/main/java/neoflex/chulkov/repository/OutboxRepository.java
@@ -1,8 +1,12 @@
 package neoflex.chulkov.repository;
 
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import neoflex.chulkov.dto.enums.OutboxStatus;
 import neoflex.chulkov.entity.Outbox;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +14,7 @@ import java.util.UUID;
 
 @Repository
 public interface OutboxRepository extends JpaRepository<Outbox, UUID> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints(@QueryHint(name = "jakarta.persistence.lock.timeout", value = "-2"))
     List<Outbox> findTop50ByStatusOrderByCreatedAtAsc(OutboxStatus status);
 }
