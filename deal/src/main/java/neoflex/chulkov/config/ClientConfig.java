873a9249785820fa08f5150da724a862c0560c30
Win122333 <chulkov_a_d@sc.vsu.ru>
2026-05-07 17:52:07 +0300

    add cloud profile

--- deal/src/main/java/neoflex/chulkov/config/ClientConfig.java
+++ deal/src/main/java/neoflex/chulkov/config/ClientConfig.java
@@ -1,6 +1,8 @@
 package neoflex.chulkov.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -10,10 +12,12 @@ public class ClientConfig {
     @Bean
     public RestClient getRestClient(
             RestClient.Builder builder,
-            @Value("${deal.calculator-base-url:http://localhost:8081}") String baseUrl
+            @Value("${deal.calculator-base-url:http://localhost:8081}") String baseUrl,
+            LoadBalancerClient loadBalancerClient
     ) {
         return builder
                 .baseUrl(baseUrl)
+                .requestInterceptor(new LoadBalancerInterceptor(loadBalancerClient))
                 .build();
     }
 }
