daf9665561f18ed79b165aa0c6dfadc36142a669
Win122333 <chulkov_a_d@sc.vsu.ru>
2026-05-07 14:26:21 +0300

    add discoveryClient to all services

--- deal/src/main/java/neoflex/chulkov/DealApplication.java
+++ deal/src/main/java/neoflex/chulkov/DealApplication.java
@@ -2,9 +2,11 @@ package neoflex.chulkov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DealApplication {
     public static void main(String[] args) {
